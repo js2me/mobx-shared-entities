c7444826ae6f8066588e2f0e56c0072ce45fc011
Sergey Volkov <js2me@outlook.com>
2024-10-04 16:34:59 +0300

    fix: 2.0.1 build

--- post-build.mjs
+++ post-build.mjs
@@ -9,28 +9,11 @@ const readFile = (file) => fs.readFileSync(file);
 const writeFile = (file, content) => fs.writeFileSync(file, content);
 //#endregion
 
-$('rm -rf tmp');
-$(`cp -r src tmp`);
-$(`cp -r LICENSE tmp`);
-
-// собираем
-$('tsc -p tsconfig.build.json');
-
-const packageJson = JSON.parse(readFile('package.json'));
+$(`cp -r LICENSE dist`);
 
 const exportsConfig = {
   './package.json': './package.json',
 };
-
-/*
-
-    ".": {
-      "import": "./dist/index.js",
-      "types": "./dist/index.d.ts"
-    },
-    "./types": "./typings/index.d.ts",
-    "./package.json": "./package.json"
-*/
 
 scanDir('src').forEach((entityName) => {
   exportsConfig[`./${entityName}`] = {
@@ -39,9 +22,17 @@ scanDir('src').forEach((entityName) => {
   };
 });
 
-packageJson.exports = exportsConfig;
-
-writeFile('tmp/package.json', JSON.stringify(packageJson, null, 2));
+writeFile(
+  'dist/package.json',
+  JSON.stringify(
+    {
+      ...JSON.parse(readFile('package.json')),
+      exports: exportsConfig,
+    },
+    null,
+    2,
+  ),
+);
 
 // execSync(`mkdir -p typings`, { stdio: 'inherit' });
 // execSync(`cp src/lib/types.ts typings/index.d.ts`, { stdio: 'inherit' });
